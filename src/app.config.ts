298fd8725a5565d7ef6c1bbb529ebcd7d4cd7359
5312 <1765080897@qq.com>
2023-05-10 13:32:58 +0800

    ci: 小程序组件懒加载

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,10 @@
 export default defineAppConfig({
-  pages: [
-    'pages/index/index'
-  ],
+  lazyCodeLoading: "requiredComponents",
+  pages: ["pages/index/index"],
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
-})
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black",
+  },
+});

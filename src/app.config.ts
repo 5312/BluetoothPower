845067925dda6d35a25120e19a94d3193181366a
5312 <1765080897@qq.com>
2023-05-17 18:18:21 +0800

    faeat: 初始化蓝牙,连接蓝牙

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,6 +1,12 @@
 export default defineAppConfig({
   lazyCodeLoading: "requiredComponents",
   pages: ["pages/index/index"],
+  requiredPrivateInfos: ["getLocation", "chooseLocation"],
+  permission: {
+    "scope.userLocation": {
+      desc: "你的位置信息将用于小程序位置接口的效果展示", // 高速公路行驶持续后台定位
+    },
+  },
   window: {
     backgroundTextStyle: "light",
     navigationBarBackgroundColor: "#fff",
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,10 +1,69 @@
-import { View, Text, Image } from "@tarojs/components";
+import { View, Text } from "@tarojs/components";
 import DevicesList from "../components/devicesList";
 import { useLoad } from "@tarojs/taro";
+import Taro from "@tarojs/taro";
 import "./index.less";
-/* assets */
-// 引用文件
-// import namedPng from "../../assets/header.png";
+
+// 判断系统蓝牙是否打开
+function isOpenBlueth() {
+  const res = Taro.getSystemInfoSync();
+  return res.bluetoothEnabled;
+}
+
+// 初始化蓝牙
+async function bluetoothStar() {
+  if (!isOpenBlueth()) {
+    Taro.showModal({
+      title: "提示",
+      content: "请打开蓝牙后重试",
+      success: function (res) {
+        if (res.confirm) {
+          Taro.navigateTo({
+            url: "/pages/index/index",
+          });
+        } else if (res.cancel) {
+          console.log("用户点击取消");
+        }
+      },
+    });
+    return;
+  }
+  let res;
+  try {
+    const { errMsg } = await Taro.openBluetoothAdapter();
+    res = errMsg;
+  } catch (e) {
+    Taro.showToast({
+      title: e.errMsg,
+      icon: "none",
+      duration: 2000,
+    });
+  }
+  // 微信小程序蓝牙未打开
+  if (Taro.getEnv() === "WEAPP" && res !== "openBluetoothAdapter:ok") {
+    // TODO 没有打开蓝牙，需要提示用户打开
+    Taro.showToast({
+      title: "蓝牙未打开",
+      icon: "none",
+      duration: 2000,
+    });
+  }
+
+  await Taro.getLocation({});
+  const data: any = await Taro.startBluetoothDevicesDiscovery({});
+  console.log(data);
+  if (data.isDiscovering || data) {
+    onDiscoveryBLE();
+  }
+}
+// 搜索蓝牙 -- 微信 支付宝搜索蓝牙设备并连接
+async function onDiscoveryBLE() {
+  let { devices } = await Taro.getBluetoothDevices();
+  //这里打印出设备列表，方便查看设备信息
+  console.log("devices", devices);
+  Taro.stopBluetoothDevicesDiscovery();
+}
+bluetoothStar();
 
 export default function Index() {
   useLoad(() => {
